b30a46a8611c666e1c639054f98491131b9c008a
Udit Kirar <105787255+UDITKIRAR@users.noreply.github.com>
2024-09-03 15:10:17 +0530

    Update App.js

--- Frontend/components/App.js
+++ Frontend/components/App.js
@@ -1,1 +1,14 @@
 
+import Navbar from '@/components/Navbar'
+import '@/styles/globals.css'
+// import { SessionProvider } from 'next-auth/react'
+// import {} from '@next/font/google'
+
+export default function App({ Component, pageProps,session }) {
+  return <>
+    {/* <SessionProvider session={session}> */}
+      <Navbar />
+      <Component {...pageProps} />
+    {/* </SessionProvider> */}
+  </>
+}
